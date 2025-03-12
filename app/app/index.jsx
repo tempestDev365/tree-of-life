380042c7808af052cb219b6bbec08d50a3ab5ee4
tempestDev365 <alfonso.christian.macatangay@gmail.com>
2025-03-12 22:21:35 +0800

    change the tsx to jsx

---
+++ app/app/index.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const app = () => {
+    return (
+        <View>
+            <Text>My first app</Text>
+        </View>
+    );
+}
+
+export default app;
