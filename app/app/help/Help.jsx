38778f13eef086c1d4f033e249333f6ede21ee7b
tempestDev365 <alfonso.christian.macatangay@gmail.com>
2025-04-07 16:20:17 +0800

    added mechanics

--- app/app/help/Help.jsx
+++ app/app/help/Help.jsx
@@ -1,20 +1,36 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, ScrollView, StyleSheet } from 'react-native';
 
-const Help = () => {
+const About = () => {
     return (
-        <View>
-            <Text style={styles.textTitle}>This feature is not available yet!!</Text>
+        <View style={styles.container}>
+            <ScrollView>
+                <Text>How it works?</Text>
+                <Text>1. User select a category ( Walking, Jogging, Running ).</Text>
+                <Text>2. The user need reach the required minimum distance and steps. </Text>
+                <Text>3. Math quiz appears after they finished the activity ( 10 seconds timer per question ).</Text>
+                <Text>4. Correct answer = Full points, Incorrect answer = 0 points</Text>
+                <Text>
+                    
+                </Text>
+            </ScrollView>
         </View>
     );
 }
 
-
 const styles = StyleSheet.create({
-    textTitle: {
-        fontSize: 30,
-        color: 'black',
-        textAlign: 'center',
-        marginTop: 50,
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 30,
+        borderWidth: 1,
+        borderRadius: 10,
+        borderColor: 'black',
+    },
+    text: {
+        fontSize: 20,
+        margin: 10,
     },
 });
-export default Help;
+
+export default About;
